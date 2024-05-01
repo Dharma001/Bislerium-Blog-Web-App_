44f38d70024a8d015e0b78c197d3e29f8faec403
Dharma001 <sapkotasajit05@gmail.com>
2024-05-01 23:25:29 +0545

    made role Curd and User Curd in bakced with modified admin dashboad with create Post

---
+++ front/src/Auths/userAuth.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+const BASE_URL = "https://localhost:7189/user/api/";
+
+export const fetchWithAuth = async (method, url, data = null) => {
+  let token = Cookies.get('token');
+  while (!token) {
+    await new Promise(resolve => setTimeout(resolve, 100));
+    token = Cookies.get('token');
+  }
+  const headers = {
+    "Authorization": `Bearer ${token}`,
+  };
+
+  if (method.toUpperCase() !== 'DELETE') {
+    if (data instanceof FormData) {
+      headers['Content-Type'] = 'multipart/form-data';
+    } else {
+      headers['Content-Type'] = 'application/json';
+    }
+  }
+
+  try {
+    const response = await axios({
+      method,
+      url: `${BASE_URL}${url}`,
+      headers,
+      data
+    });
+    return response;
+  } catch (error) {
+    throw new Error(error.response?.data?.message || "Request failed");
+  }
+};
