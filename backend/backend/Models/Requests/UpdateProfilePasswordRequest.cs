c0b2b4bb2d4bda46b2d3ff8a766ec9cc6482eb90
Dharma001 <sapkotasajit05@gmail.com>
2024-05-03 21:46:43 +0545

    made user profile

---
+++ backend/backend/Models/Requests/UpdateProfilePasswordRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.Models.Requests
+{
+    public class UpdateProfilePasswordRequest
+    {
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+
+}
