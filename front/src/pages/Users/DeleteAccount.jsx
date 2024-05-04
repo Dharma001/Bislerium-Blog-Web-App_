2e0fdaf37855822e987c30749f02db17c536c2fc
[Sumit Chuhan] <[valid-email]>
2024-05-04 12:49:30 +0545

    added the delete account page content.

--- front/src/pages/Users/DeleteAccount.jsx
+++ front/src/pages/Users/DeleteAccount.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 function DeleteAccount() {
   return (
-    <div>DeleteAccount</div>
+    <div>
+      <h1><strong><big>Confirmation: Delete Account</big></strong></h1>
+      <br></br>
+      <p>Are you sure you want to delete your account? This action cannot be undone.</p>
+      <p>By deleting your account, you will lose access to all your data and any associated services.</p>
+      <br></br><p>Please consider the following:</p>
+      <br></br>
+      <ul>
+        <li>1. Once deleted, your account information, including your profile, settings, and history, will be permanently removed from our system.</li>
+        <li>2. You will no longer be able to log in or access any features tied to your account.</li>
+      </ul>
+      <br></br>
+      <p>If you're certain about deleting your account, please click "Confirm Delete" below. Otherwise, click "Cancel" to retain your account.</p>
+      
+    </div>
   )
 }
 
